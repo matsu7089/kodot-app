35855e8fe4c3678959875c96a7ca2f0663f3d872
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2023-11-12 05:06:07 +0000

    add: astro:contentの設定を追加

---
+++ src/content/config.ts
@@ -0,0 +1,25 @@
+import { defineCollection, z } from 'astro:content'
+
+const baseSchema = z.object({
+  isDraft: z.boolean().default(false),
+  title: z.string(),
+  pubDate: z.date(),
+  tags: z.array(z.string()).default(['others']),
+})
+
+const articles = defineCollection({
+  type: 'content',
+  schema: baseSchema.extend({
+    godotVersion: z.string(),
+  }),
+})
+
+const notes = defineCollection({
+  type: 'content',
+  schema: baseSchema,
+})
+
+export const collections = {
+  articles,
+  notes,
+}
