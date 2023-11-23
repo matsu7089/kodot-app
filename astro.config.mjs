395307cb72d02b24bae0fa3dbe435431149ed26b
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2023-11-24 03:27:48 +0900

    add: remarkModifiedTimeを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
+import { execSync } from 'child_process'
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import remarkCodeTitles from 'remark-flexible-code-titles'
+
+// ref: https://docs.astro.build/ja/recipes/modified-time/
+const remarkModifiedTime = () => {
+  return (_tree, file) => {
+    const filepath = file.history[0]
+    const result = execSync(`git log -1 --pretty="format:%cI" ${filepath}`)
+    file.data.astro.frontmatter.lastModified = result.toString()
+  }
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,6 +20,6 @@ export default defineConfig({
       footnoteLabelTagName: 'span',
       footnoteLabel: '脚注',
     },
-    remarkPlugins: [remarkCodeTitles],
+    remarkPlugins: [remarkCodeTitles, remarkModifiedTime],
   },
 })
