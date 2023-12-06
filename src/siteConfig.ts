fa1cdc7b1c29dc8a9398a58772e9645dcc617692
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2023-12-07 02:10:41 +0900

    update: primaryColorの管理方法を変更

--- src/siteConfig.ts
+++ src/siteConfig.ts
@@ -3,3 +3,18 @@ export const pageSize = 12
 
 /** 日付表示フォーマット */
 export const dateFormat = 'YYYY-MM-DD'
+
+// theme
+export type PrimaryColor = 'blue' | 'green'
+
+type ClassMap = Record<PrimaryColor, string>
+
+export const primaryTextClass = {
+  blue: 'text-kodot-blue',
+  green: 'text-kodot-green',
+} as const satisfies ClassMap
+
+export const primaryBgClass = {
+  blue: 'bg-kodot-blue dark:bg-kodot-blue-dark',
+  green: 'bg-kodot-green dark:bg-kodot-green-dark',
+} as const satisfies ClassMap
