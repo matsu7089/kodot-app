4cc5df6ca0d1077c50f40b4458bc9c7c220fe7ce
matsu7089 <32781959+matsu7089@users.noreply.github.com>
2024-07-12 07:44:00 +0900

    fix: lastModifiedが無いとき空文字ではなくundefinedになるように修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ const remarkModifiedTime = () => {
   return (_tree, file) => {
     const filepath = file.history[0]
     const result = execSync(`git log -1 --pretty="format:%cI" ${filepath}`)
-    file.data.astro.frontmatter.lastModified = result.toString()
+    file.data.astro.frontmatter.lastModified = result.toString() || undefined
   }
 }
 
